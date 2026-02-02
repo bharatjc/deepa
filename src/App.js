4edc2c0309e61702a783ff6f9de7a94633fafaec
Bharat Chaudhary <bhrt.7856@gmail.com>
2026-02-02 21:25:28 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <p>I love you Deepa</p>
+        <a>
+          I feel positive, confident, happy, and complete when I’m with you.
         </a>
       </header>
     </div>
